0fc34bd0190a7eceb71f90f09204a05b3473706e
Pavel Dashko <dashko.pavel@bk.ru>
2023-05-21 20:55:14 +0300

    Step by step

--- main/Source.cpp
+++ main/Source.cpp
@@ -5,6 +5,8 @@ bool GameOver;
 const int width = 30;
 const int height = 30;
 int x, y, fruitX, fruitY, score;
+int taliX[100], taliY[100];
+int nTail;
 enum class eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
 eDirection dir;
 
@@ -12,10 +14,10 @@ void SetUp()
 {
 	GameOver = false;
 	dir = eDirection::STOP;
-	x = width / 2 - 1;
-	y = height / 2 - 1;
-	fruitX = rand() % width;
-	fruitX = rand() % height; 
+	x = width/2 - 1;
+	y = height/2 - 1;
+	fruitX = rand() % (width -1);
+	fruitY = rand() % (height-1); 
 	score = 0;
 }
 void Draw()
@@ -34,16 +36,22 @@ void Draw()
 			{
 				std::cout << "#";
 			}
-			if (i == x && j == y)
+			if (i == y && j == x)
 			{
 				std::cout << "0";
 			}
-			else if (i == fruitX && j == fruitX)
+			else if (i == fruitY && j == fruitX)
 			{
 				std::cout << "F";
 			}
 			else
+			{
+				for (int k = 0; k < nTail; k++) {
+					if (taliX[k] == j && taliY[k] == i)
+						std::cout << 'o';
+				}
 				std::cout << " ";
+			}
 		}
 		std::cout << std::endl;
 	}
@@ -52,11 +60,13 @@ void Draw()
 		std::cout << "#";
 	}
 	std::cout << std::endl;
+	std::cout << "YOUR SCORE : " << score << std::endl;
 }
 void Input()
-{
+ {
 	if (_kbhit())
 	{
+		// std::cout << _getch();
 		switch (_getch())
 		{
 		case 'a':
@@ -80,20 +90,42 @@ void Input()
 
 void Logic()
 {
+	int prevx = taliX[0];
+	int prevy = taliY[0];
+	int prev2x, prev2y;
+	for (int i = 1; i < nTail; i++) {
+		prev2x = taliX[i];
+		prev2y = taliY[i];
+		taliX[i] = prevx;
+		taliY[i] = prevy;
+		prevx = prev2x;
+		prevy = prev2y;
+	}
 	switch(dir)
 	{ 
 		case eDirection::LEFT:
 			x--;
+			// std::cout << x << " ";
 			break;
 		case eDirection::RIGHT:
 			x++;
 			break;
 		case eDirection::UP:
-			y++;
+			y--;
+			// std::cout << y << " ";
 			break;
 		case eDirection::DOWN:
-			y--;
+			y++;
+			// std::cout << y << " ";
 			break;
+	}
+	if (x > width || x < 0 || y > height || y < 0)
+		GameOver = true;
+	if (x == fruitX && y == fruitY) {
+		score += 100;
+		fruitX = rand() % (width -1);
+		fruitY = rand() % (height -1);
+		nTail++;
 	}
 }
 
